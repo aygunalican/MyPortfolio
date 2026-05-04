d5ac57afce4461efbbf8aa0892b4befb5f9734d0
aygunalican <elicanovaaygun@gmail.com>
2026-05-04 13:47:48 +0400

    second version

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,36 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../assets/css/contact.css'
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+  const form=useRef();
+  
+  const sendEmail=(e)=>{
+e.preventDefault();
+
+emailjs .sendForm(
+  "service_xzcvowz",
+  "template_dy1o6l2",
+  form.current,
+ "lUvC68bDh2qD8mOCP"
+) .then(()=>{alert("Successfully")},
+(error)=>{console.log(error.text)}
+  
+ ); }
   return (
     <section id='contact' className="contact">
   <div className="contact-con">
       <h2>Lets work together!</h2>
 <p>I’m always open to new ideas, projects, and collaborations. Get in touch!</p>
 
-      <form className="contact-form">
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
         <div className="wow">
           <div className="field ">
             <label>Your name *</label>
-            <input type="text" />
+            <input name="from_name" type="text" />
           </div>
 
           <div className="field">
             <label>Your email *</label>
-            <input type="email" />
+            <input name="from_email" type="email" />
           </div>
         </div>
 
         <div className="field subj">
           <label>Choose service</label>
-          <input type="text" />
+          <input name="from_service" type="text" />
         </div>
 
         <div className="field mesg">
           <label>Your message</label>
-          <textarea rows="5"></textarea>
+          <textarea name="message" rows="5"></textarea>
         </div>
 
-        <button type="button">Send Message</button>
+        <button type="submit">Send Message</button>
       </form>
     </div>
 </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,11 @@ const Home = () => {
         <h1 className='typewriter' >I am Aygun Alijanova—<span>Front-end Developer.</span>
          
         </h1>
-<p>
+{/* <p>
 I create modern, responsive, and user-friendly digital experiences. 
 My focus is on crafting seamless interfaces that are intuitive, engaging, and visually appealing, 
 bringing ideas to life in the most elegant way.
-</p>
+</p> */}
       </div>
       <div className="img-con">
         <img src="https://i.pinimg.com/originals/12/e8/a6/12e8a6a547e317524121f7a5d6084036.gif" />
