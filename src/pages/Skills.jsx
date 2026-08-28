f3c6c4c4bf3986cc81c7c39b120a0251dbf2c800
aygunalican <elicanovaaygun@gmail.com>
2026-08-28 09:40:06 +0400

    Update Skills.jsx

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,32 +5,26 @@ export default function Skills() {
   const skillsData = [
     {
       name: "HTML",
-      percent: 92,
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
     },
     {
       name: "CSS3",
-      percent: 80,
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
     },
     {
       name: "JavaScript",
-      percent: 85,
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     },
     {
       name: "GitHub",
-      percent: 99,
       icon: "https://toppng.com/uploads/preview/github-logo-transparent-png-11659780101agvzsukgqz.png",
     },
     {
       name: "ReactJS",
-      percent: 89,
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     },
     {
       name: "BootStrap",
-      percent: 93,
       icon: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/bootstrap-5-logo-icon.png",
     },
   ];
@@ -46,7 +40,6 @@ export default function Skills() {
         {skillsData.map((skill, index) => (
           <div className="skill-card" key={index}>
             <img src={skill.icon} alt={skill.name} />
-            <h3>{skill.percent}%</h3>
             <p>{skill.name}</p>
           </div>
         ))}
